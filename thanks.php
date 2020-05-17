59c8719115c6d3c77eaf3c23a592fc109c5e2e0a
takuma10Mi <amaebilove@gmail.com>
2020-05-17 22:11:35 +0900

    thanksページ不正アクセス処理

--- thanks.php
+++ thanks.php
@@ -1,4 +1,9 @@
 <?php
+  if($_SERVER["REQUEST_METHOD"] != "POST"){
+  header('Location: index.php'); 
+  exit;
+  }
+  echo "お問い合わせありがとうございました。"."<br>";
   $link = mysqli_connect("localhost", "root", "root","memberapp");
   if( $link->connect_errno ) {
     echo $link->connect_errno . ' : ' . $link->connect_error;
@@ -9,10 +14,6 @@
   $sql = 'SELECT * FROM users WHERE id = (SELECT MAX(id) FROM users)' ;
   $result = $link->query($sql);
   
-  if( $result ) {
-    // fetch_all結果データを全件まとめて配列で取得
-    var_dump($result->fetch_all());
-  }
   // データベースとの接続を解除
   $link->close();
 ?>
